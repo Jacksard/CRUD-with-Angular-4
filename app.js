c967cca06ebb28c8c2bb2486f65c6fb9647c62a7
jacobgoskha <sardaukarkk@gmail.com>
2017-09-07 16:11:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+var port = process.env.PORT || 8080;
+
+app.listen(port, function(){
+	console.log('Server started on port ' + port);
+});
