b757f601ef1ad516742a0b736c17d9eaa0e2c75f
jacobgoskha <sardaukarkk@gmail.com>
2017-09-07 16:17:19 -0400

    first launch

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+
+var port = process.env.PORT || 8080;
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
-// Heroku port
-var port = process.env.PORT || 8080;
+
+
 
 app.listen(port, function(){
 	console.log('Server started on port ' + port);
